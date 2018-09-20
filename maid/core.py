d3c86127e70176d1b25ced7c808e3cbd44de8118
Nguyễn Tuấn Dũng <ntddebugger@gmail.com>
2018-09-20 08:42:53 +0700

    Add download function

--- maid/core.py
+++ maid/core.py
@@ -1,8 +1,8 @@
 # Libraries
-import wget
 import requests
 import zipfile
 import os
+from urllib.parse import urlunparse
 
 # Modules
 import pkgman
@@ -12,16 +12,34 @@ import config
 # TODO: Add function
 
 
+def download(url, name=""):
+    filename = name if name else url.split('/')[-1]
+    with requests.get(url, stream=True) as r:
+        with open(filename, 'wb') as f:
+            for chk in r.iter_content(chunk_size=1024):
+                if chk:
+                    f.write(chk)
+    return filename
+
+
 def get(package):
     """Retrieve package with specified url"""
 
+    cwd = os.getcwd()
     os.chdir(config.maidTempDir)
-    filename = wget.download(package.bin_url)
+    filename = download(urlunparse(package.bin_url))
     print(f'Downloaded {filename}')
-
+    os.chdir(cwd)
 
 # def query(package, pkg_list):
 # TODO: Query function
 
 # def rem(package)
 # TODO: Remove function
+
+
+t = pkgman.pkg('2048', '5.2.4',
+               'https://github.com/taptapking/2049/releases/download/5.3/2048.5.3.x86.exe')
+
+get(t)
+# download('https://github.com/taptapking/2049/releases/download/5.3/2048.5.3.x86.exe')
