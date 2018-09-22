bf66e11cfc83cf7de9f293eb71217db804b85c46
Nguyễn Tuấn Dũng <ntddebugger@gmail.com>
2018-09-22 21:16:15 +0700

    Rewrite pkgman

--- maid/pkgman.py
+++ maid/pkgman.py
@@ -1,38 +1,50 @@
 # Libraries
 import json
 import os
-import fire
 from urllib.parse import urlparse
 
 
 class pkg:
-    def __init__(self, name, ver, bin_url, sha1=""):
+    def __init__(self, name, bin_url, sha1, is_local=False):
         self.name = str(name)
-        self.ver = str(ver)
         self.bin_url = urlparse(bin_url)
         self.sha1 = str(sha1)
+        self.is_local = bool(is_local)
 
-    def gen(self, override=False):
-        print('Generating package')
-        if not os.path.isdir(self.name):
-            os.mkdir(self.name)
-        with (open(f'{self.name}\\task', 'w')) as pkg:
-            json.dump({'name': self.name,
-                       'ver': self.ver,
-                       'bin_url': self.bin_url,
-                       'sha1': self.sha1},
-                      pkg)
+    def toJSON(self):
+        return json.dumps(self, default=lambda o: o.__dict__,
+                          indent=4)
+
+    @staticmethod
+    def is_pkg(p_json):
+        if type(p_json) is dict:
+            if not all (key in p_json for key in ('name', 'bin_url', 'sha1')):
+                return False
+        return True
 
     @classmethod
-    def read(folder):
-        print('Reading package')
-        if not os.path.isdir(str(folder)):
-            with (open(f'{str(folder)}\\task', 'r')) as pkg:
-                db = json.load(pkg)
-            if str(db['name']) == str(folder):
-                return db
+    def fromJSON(cls, p_json):
+        if pkg.is_pkg(p_json):
+            dmp = json.loads(p_json)
+        return cls(dmp['name'], dmp['bin_url'], dmp['sha1'])
+        # TODO: is_local
+
+class pkg_list:
+    def __init__(self, filename):
+        self.data = []
+        data = json.load(filename)
+        if type(data) is list:
+            for p in data:
+                if pkg.is_pkg(p):
+                    self.data.append(pkg.fromJSON(p))
+
+    def add(self, package: pkg):
+        self.data.append(pkg)
+
+    def rem(self, package: pkg):
+        self.data.remove(pkg)
+    
+    # TODO: Low-level API
 
 # Example
-# t = pkg('2048', '5.2.4',
-#         'https://github.com/taptapking/2048/releases/download/5.2.4/2048.5.2.4.x86.exe')
-# t.gen()
+# print(pkg('2049', 'https://www.example.com/', 'sha1').toJSON())
