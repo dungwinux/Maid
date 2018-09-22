26537e05cffab61a5ac25e42dc1bd6830da3f43d
Nguyễn Tuấn Dũng <ntddebugger@gmail.com>
2018-09-22 23:42:13 +0700

    Implement get API

--- maid/config.py
+++ maid/config.py
@@ -1,7 +1,8 @@
-#libraries
+# libraries
 import os
 
 appPath = os.getenv('APPDATA')
 maidDir = os.fsencode(os.getcwd() + '\\maid\\')
 maidConfDir = os.fsencode(appPath + '\\maid\\')
 maidTempDir = os.fsencode(os.getenv('TEMP') + '\\maid\\')
+maidPackDir = os.fsencode(os.fsdecode(maidDir) + 'pkg\\')
--- maid/core.py
+++ maid/core.py
@@ -2,11 +2,12 @@
 import zipfile
 import os
 import wget
+import shutil
 from urllib.parse import urlunparse
 
 # Modules
 import pkgman
-from config import maidDir, maidTempDir
+from config import maidDir, maidTempDir, maidPackDir
 
 # def add(package):
 # TODO: Add function
@@ -18,9 +19,20 @@ def get(url):
     os.chdir(maidTempDir)
     filename = wget.download(url)
     print(f'\nDownloaded {filename}')
+    pkgname = input(f'Package name (default is {filename}): ')
+    if not pkgname:
+        pkgname = filename
+    extractPath = os.fsdecode(maidPackDir) + pkgname + '\\'
+    print(f'Extracting to {extractPath}')
+    if not os.path.isdir(extractPath):
+        os.makedirs(extractPath)
+    if zipfile.is_zipfile(filename):
+        zipfile.ZipFile(filename).extractall(path=extractPath)
+    else:
+        print('Not zip archive file. Casting downloaded file as extracted package.')
+        shutil.copyfile(filename, os.fsencode(extractPath))
 
     os.chdir(maidDir)
-
 
 
 # def query(package, pkg_list):
@@ -28,5 +40,4 @@ def get(url):
 
 # def rem(package)
 # TODO: Remove function
-
-# get('https://github.com/taptapking/2049/releases/download/5.3/2048.5.3.x86.exe')
+get('https://github.com/taptapking/2049/releases/download/5.3/2048.5.3.x86.exe')
