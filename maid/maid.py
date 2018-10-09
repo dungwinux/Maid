17e3c46d2a830e92dab832e7430c9d8b94ef0d8d
Nguyễn Tuấn Dũng <ntddebugger@gmail.com>
2018-10-09 18:59:37 +0700

    Rewrite under Fire interface

--- maid/maid.py
+++ maid/maid.py
@@ -3,24 +3,31 @@ import os
 import argparse
 import configparser
 import shutil
+import urllib
+import fire
 
 # Modules
 import core
-# from config import maidConfDir, maidDir, maidPackDir, maidTempDir, appPath
 
-# Global variable
+# Argument parsing
 
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        prog='maid', description='Maid - Package Manager')
+    parser.add_argument('--version', action='version',
+                        version='%(prog)s Pre-Alpha')
+    subparsers = parser.add_subparsers(
+        help='sub-command help',
+        title='command',
+        required=True,
+        metavar='<command>')
 
-parser = argparse.ArgumentParser(
-    prog='Maid', description='Maid - Package Manager')
-parser.add_argument('--version', action='version',
-                    version='%(prog)s Pre-Alpha')
-subparsers = parser.add_subparsers(
-    help='sub-command help',
-    title='command',
-    required=True,
-    metavar='<command>')
-subparsers.add_parser('add', help='Add package')
-subparsers.add_parser('rem', help='Remove package')
+    add_parse = subparsers.add_parser('add', help='Add package')
+    add_parse.add_argument('url', type=str, help='Url to package', metavar='<url>')
+    # add_parse.set_defaults(func=core.get)
 
-args = parser.parse_args()
+    rem_parse = subparsers.add_parser('rem', help='Remove package')
+    rem_parse.add_argument('pkg_name', type=str, help='Package name', metavar='<pkg_name>')
+    # rem_parse.set_defaults(func=core.)
+
+    # args = parser.parse_args()
