7b6d340bde66f217c2301e03ab3c12b2694cc372
Nguyễn Tuấn Dũng <ntddebugger@gmail.com>
2018-10-17 20:04:18 +0700

    Add comments to maid.py

--- maid/maid.py
+++ maid/maid.py
@@ -12,24 +12,29 @@ import core
 # Argument parsing
 
 if __name__ == "__main__":
+    # Declare Argument Parser
     parser = argparse.ArgumentParser(
         prog='maid',
         description='Maid - Package Manager',
         epilog='Example: TBA')
+    # --version argument
     parser.add_argument(
         '--version',
         action='version',
         version='%(prog)s Pre-Alpha')
+    # TODO: --force argument
     parser.add_argument(
         '--force',
         action='store_true'
     )
+    # Add subparser to main Parser
     subparsers = parser.add_subparsers(
         help='sub-command help',
         title='command',
         required=True,
         metavar='<command>')
 
+    # add sub-command
     add_parse = subparsers.add_parser('add', help='Add package')
     add_parse.add_argument(
         'package',
@@ -39,6 +44,7 @@ if __name__ == "__main__":
     # nargs='+'
     add_parse.set_defaults(func=core.get)
 
+    # rem sub-command
     rem_parse = subparsers.add_parser('rem', help='Remove package')
     rem_parse.add_argument(
         'package',
@@ -48,6 +54,7 @@ if __name__ == "__main__":
     # nargs='+'
     rem_parse.set_defaults(func=core.rem)
 
+    # query sub-command
     que_parse = subparsers.add_parser('que', help='Query package')
     que_parse.add_argument(
         'package',
@@ -57,11 +64,16 @@ if __name__ == "__main__":
         nargs='?')
     que_parse.set_defaults(func=core.query, package='.*?')
 
+    # clear sub-command
     cle_parse = subparsers.add_parser(
         'cle', help='Clear temporary package downloads')
     cle_parse.set_defaults(func=core.clear)
 
+    # Call argument parser
     args = parser.parse_args()
+    # Checking for argument
+    # If no argument -> clear
+    # else           -> other function
     if 'package' in args:
         args.func(args.package)
     else:
