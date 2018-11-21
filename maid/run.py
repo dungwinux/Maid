9e95d81e9042efd53e8e846e38080ebcecf1c7b1
Nguyễn Tuấn Dũng <ntddebugger@gmail.com>
2018-11-22 05:45:49 +0700

    Modulizing function

--- maid/run.py
+++ maid/run.py
@@ -1,38 +1,45 @@
 import os
 import shutil
-from config import maidDir, maidPackDir
+from config import maidDir, maidPackDir, maidBinDir
+
+
+def bin_walk(pkg_name, fun):
+    src = os.path.join(os.fsdecode(maidPackDir), pkg_name) + '\\'
+    for path, dirs, files in os.walk(src):
+        if path[len(src) + 1:].count(os.sep) < 2:
+            for file in files:
+                if os.path.splitext(file)[-1] in ['.exe', '.reg', '.bat']:
+                    fun(path, file)
 
 
 def bin_search(pkg_name):
     """Search binaries and executables then hard-link to bin/ folder"""
 
     print("""Searching for binaries & executables ...""")
-
-    src = os.path.join(os.fsdecode(maidPackDir), pkg_name) + '\\'
-    maidBinDir = os.path.join(os.fsdecode(maidDir), 'bin')
-
-    for path, dirs, files in os.walk(src):
-        for file in files:
-            if os.path.splitext(file)[-1] in ['.exe', '.reg', '.bat']:
-                binary = os.path.join(path, file)
-                print("Found:", binary)
-                os.link(binary, os.path.join(maidBinDir, file))
-                print("\t->", os.path.join(maidBinDir, file))
+    bin_walk(pkg_name, linkBin)
 
 
 def link_rem(pkg_name):
     """Search hard-link of binaries and executables then delete"""
 
     print("""Searching for binaries & executables ...""")
+    bin_walk(pkg_name, unlinkBin)
 
-    src = os.path.join(os.fsdecode(maidPackDir), pkg_name) + '\\'
-    maidBinDir = os.path.join(os.fsdecode(maidDir), 'bin')
 
-    for path, dirs, files in os.walk(src):
-        for file in files:
-            if os.path.splitext(file)[-1] in ['.exe', '.reg', '.bat']:
-                link = os.path.join(maidBinDir, file)
-                print(link)
-                if os.path.isfile(link):
-                    print("Unlink:", link)
-                    os.remove(link)
+def linkBin(path, file):
+    binary = os.path.join(path, file)
+    link = os.path.join(os.fsdecode(maidBinDir), file)
+    print("Found:", binary)
+    try:
+        os.link(binary, link)
+        print("\t->", link)
+    except FileExistsError:
+        print("Link is valid, can't re-link")
+
+
+def unlinkBin(path, file):
+    link = os.path.join(os.fsdecode(maidBinDir), file)
+    print(link)
+    if os.path.isfile(link):
+        print("Unlink:", link)
+        os.remove(link)
