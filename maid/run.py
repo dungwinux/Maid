5b98bc442b16f0f40dc8142e02a25a1287113187
Nguyễn Tuấn Dũng <ntddebugger@gmail.com>
2018-11-22 06:01:58 +0700

    Remove unused, add comment

--- maid/run.py
+++ maid/run.py
@@ -1,15 +1,24 @@
 import os
-import shutil
-from config import maidDir, maidPackDir, maidBinDir
+from config import maidPackDir, maidBinDir
 
 
 def bin_walk(pkg_name, fun):
+    """Recursively browse for executables from pkg_name directory"""
+
     print("""Searching for binaries & executables ...""")
+    # Maximum traversal depth
+    MAX_DEPTH = 2
+    # Generate location to search
     src = os.path.join(os.fsdecode(maidPackDir), pkg_name) + '\\'
+    # Use os.walk to browse recursively
     for path, dirs, files in os.walk(src):
-        if path[len(src) + 1:].count(os.sep) < 2:
+        # Check for traversal depth
+        if path[len(src) + 1:].count(os.sep) < MAX_DEPTH:
+            # Browsing through file in directory
             for file in files:
+                # Filter executables
                 if os.path.splitext(file)[-1] in ['.exe', '.reg', '.bat']:
+                    print("Found:", file)
                     fun(path, file)
 
 
@@ -26,21 +35,27 @@ def link_rem(pkg_name):
 
 
 def linkBin(path, file):
+    # This is file that need to be linked
     binary = os.path.join(path, file)
+    # This is where file will be linked to
     link = os.path.join(os.fsdecode(maidBinDir), file)
-    print("Found:", binary)
     try:
+        # Try to link file in case no link with same name
         os.link(binary, link)
         print("\t->", link)
     except FileExistsError:
-        print("Link is valid, can't re-link")
+        # If link is available, inform user
+        print("Link is valid, cannot re-link")
 
 
 def unlinkBin(path, file):
+    # This is where file will be unlinked
     link = os.path.join(os.fsdecode(maidBinDir), file)
-    print(link)
+    # Check if link is available in bin/ folder
     if os.path.isfile(link):
+        # If yes, delete it
         print("Unlink:", link)
         os.remove(link)
     else:
+        # Else, inform "Missing link"
         print("Missing link:", link)
