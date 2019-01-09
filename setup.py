56a969a09564797f2e57bb3a68b8467f9911aa1c
Nguyễn Tuấn Dũng <ntddebugger@gmail.com>
2019-01-09 15:33:40 +0700

    Format code

--- setup.py
+++ setup.py
@@ -1,20 +1,20 @@
 import setuptools
 
 with open("README.md", "r") as readline:
-  long_description = readline.read()
+    long_description = readline.read()
 
 setuptools.setup(
-  name = "Maid",
-  version = "0.0.0",
-  author = "dungwinux, minhducsun2002",
-  author_email = "",
-  long_description = long_description,
-  long_description_content_type = "text/markdown",
-  url = "https://github.com/dungwinux/Maid",
-  packages = setuptools.find_packages(),
-  classifiers = [
-    "Programming Language :: Python :: 3.7",
-    "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
-    "Operating System :: Microsoft :: Windows"
-  ],
+    name="Maid",
+    version="0.0.0",
+    author="dungwinux, minhducsun2002",
+    author_email="",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dungwinux/Maid",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: Microsoft :: Windows"
+    ],
 )
